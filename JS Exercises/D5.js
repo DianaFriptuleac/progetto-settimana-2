0cea9427b0ea6a73aa6c03de0ed4ea3c7b15c5b5
DianaFriptuleac <168530400+DianaFriptuleac@users.noreply.github.com>
2024-06-21 22:30:54 +0700

    commit

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -70,24 +70,50 @@ const cars = [
   },
 ]
 console.log('*****Esercizio5*****');
-cars.licensePlat[''];
-console.log(cars);
+
+cars.licensePlat = "AR231IT";
+console.log (cars);
+
 
 
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
+console.log('*****Esercizio6*****');
+let newCar = {
+brand: 'Kia',
+model: 'Stonic',
+color: 'black',
+trims: [ 'style', 'confort'],
+}
+cars.push(newCar);
+console.log(cars);
+
+/*cars.trims.pop();
+console.log (cars);*/
+
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
-const justTrims = []
+console.log('*****Esercizio7*****');
+const justTrims = [];
+cars.trims === justTrims[0];
+console.log(justTrims);
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
+console.log('*****Esercizio8*****');
+
+if(cars.color[i].charAt (0) === 'b'){
+  console.log('Fizz');
+}else {
+  console.log('Buzz');
+}
+
 
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
